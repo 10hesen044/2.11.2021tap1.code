e097d9dff84b3f50669e9bf952453c5c30fd76e2
10hesen044 <tu201906250@code.edu.az>
2021-11-02 11:47:18 +0400

    project added

---
+++ 2.11.2021tap1/2.11.2021tap1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2._11._2021tap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String deyerini girin:");
+            string text = Console.ReadLine();
+            GetFan(text);
+        }
+        static void GetFan(string space)
+        {
+            space = space.Trim();
+            for (int i = 0; i < space.Length; i++)
+            {
+                while (space[i] == ' ' && space[i + 1] == ' ')
+                {
+                    space = space.Remove(i, 1);
+                }
+
+            }
+            Console.WriteLine(space);
+        }
+    }
+}
